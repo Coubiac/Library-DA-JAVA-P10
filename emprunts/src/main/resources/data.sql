c81565125124d4e75bf1d6f29092e1cb1b576765
Coubiac <benoit.grisot@gmail.com>
2020-10-17 10:12:28 +0200

    Ajout d'un système de validation des emprunts

--- emprunts/src/main/resources/data.sql
+++ emprunts/src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO emprunt_entity (id, date_emprunt, date_retour, exemplaire_barcode, user_id, is_extended) VALUES
-(1, '2020-06-01 14:05:24', NULL, '819003', 'b9cb236e-912c-468c-a636-ae1f4fe4723b', '0');
+(1, '2020-10-01 14:05:24', NULL, '819003', 'b9cb236e-912c-468c-a636-ae1f4fe4723b', '0');
